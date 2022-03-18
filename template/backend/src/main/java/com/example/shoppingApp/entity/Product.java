4c042e1ad2a3efaab10efa2afcf75e78410284d2
grizgy <e1427646@student.tuwien.ac.at>
2022-03-18 23:49:37 +0200

    Getters and setters

--- template/backend/src/main/java/com/example/shoppingApp/entity/Product.java
+++ template/backend/src/main/java/com/example/shoppingApp/entity/Product.java
@@ -1,5 +1,7 @@
 package com.example.shoppingApp.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Product {
 
 
@@ -14,8 +16,107 @@ public class Product {
     private String color;
     private int quantity;
 
+    public Product (@JsonProperty("id")Long id,
+                 @JsonProperty("title")String title,
+                 @JsonProperty("price")double price,
+                 @JsonProperty("description")String description,
+                 @JsonProperty("category")String category,
+                 @JsonProperty("image")String image,
+                 @JsonProperty("rating")int[] rating,
+                 @JsonProperty("size")String size,
+                 @JsonProperty("color")String color,
+                 @JsonProperty("size")int quantity) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.description = description;
+        this.category = category;
+        this.image = image;
+        this.rating = rating;
+        this.size = size;
+        this.color = color;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription (String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 
 
+    public String getImage() {
+        return image;
+    }
 
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int[] getRating() {
+        return rating;
+    }
+
+    public void setRating(int[] rating) {
+        this.rating = rating;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String category) {
+        this.color = color;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 
 }
--- template/backend/src/main/java/com/example/shoppingApp/entity/User.java
+++ template/backend/src/main/java/com/example/shoppingApp/entity/User.java
@@ -1,12 +1,13 @@
 package com.example.shoppingApp.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Date;
 
-public class User {
+@EntityScan
+public class User implements Serializable {
 
     private Long id;
     private String email;
@@ -34,6 +35,69 @@ public class User {
         this.lastname = lastname;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Collection getProducts() {
+        return products;
+    }
+
+    public void setProducts(Collection products) {
+        this.products = products;
+    }
 
 
 }
